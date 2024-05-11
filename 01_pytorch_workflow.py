d16c37f7f0b2338eff103e401426a09dadbdbe31
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-05-12 00:31:31 +0600

    exercise

--- 01_pytorch_workflow.py
+++ 01_pytorch_workflow.py
@@ -606,9 +606,56 @@ torch.__version__
 This means if we've got access to a GPU, our code will use it (for potentially faster computing)
 
 If no GPU is available, the code will default to using CPU
-
-# 6.1
-Create
 """
 
-# 6.1
+# Setup device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
+"""# 6.1 Data|"""
+
+# Create some data using linear regression formula y=weigh * X + bias
+weight = 0.7
+bias = 0.3
+
+# Create range of values
+start = 0
+end = 1
+step = 0.02
+
+# Create X and y (features and labels)
+X = torch.arange(start, end, step).unsqueeze(dim=1) # withour unsqueeze error will pop up
+y = weight * X + bias
+
+X[:10], y[:10]
+
+# Split data
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+len(X_train), len(y_train), len(X_test), len(y_test)
+
+# Create a function for plot predictions (Plot training data, test data and compares predictions.)
+def plot_prediction (
+                      train_data= X_train,
+                      train_labels= y_train,
+                      test_data= X_test,
+                      test_labels= y_test,
+                      predictions= None
+                    ):
+  # Plot training data -> blue color
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training Data")
+
+  # Plot test data -> gree color
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Test Data")
+
+  # Check if there are predictions
+  if predictions is not None:
+    # Plot the predictions if they exist
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14})
+
+plot_prediction()
+
